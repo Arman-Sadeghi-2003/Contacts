42a4d1dac24fadb8b9775a75021e504853bca8bc
Arman Sadeghi <Terminator_arman@yahoo.com>
2024-11-07 05:42:08 -0800

    Initialized Git

---
+++ Contacts/Contacts/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Contacts.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
